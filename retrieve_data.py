839d4229e80f53152291c1c08f58996ebf01cfff
TJ Swidorski <your_email@example.com>
2026-03-20 17:51:01 -0400

    Fix filename

--- retrieve_data.py
+++ retrieve_data.py
@@ -9,12 +9,12 @@ load_dotenv()
 
 os.makedirs("data", exist_ok=True)
 
-df = kagglehub.load_dataset(
+df = kagglehub.dataset_load(
     KaggleDatasetAdapter.PANDAS,
     "fedesoriano/stroke-prediction-dataset",
-    "",
+    "healthcare-dataset-stroke-data.csv"
 )
 
 df.to_csv("data/stroke_data.csv", index=False)
 print(f"Saved {len(df)} rows to data/stroke_data.csv")
-print(df.head())
+# print(df.head())
